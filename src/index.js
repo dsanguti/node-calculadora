ea93e7d5b785a4b9664429be05e05ea85125ee0b
dsanguti <dsanguti@gmail.com>
2022-08-11 12:34:51 +0200

    Capturar entrada del usuario

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import { printHello } from '#Lib/hello.js';
+import { promptQuestion } from '#Lib/promptQuestion.js';
 
-printHello();
+// 1º captura entrada de usuario por consola
+(async () => {
+    const userAnswer = await promptQuestion('Introduce tu operación: ');
+    console.log(userAnswer);
+})();
+// 2º Validar enrada y separar las partes de la misma en operandos y operaciones
+// 3º Realizar la operación
+// 4º Mostrar resultado por consola
