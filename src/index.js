7ce8519862468b7a7a6abec3c16850d288ac34f7
dsanguti <dsanguti@gmail.com>
2022-08-12 10:20:58 +0200

    Refactor y tratamiento errores

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import { promptQuestion } from '#Lib/promptQuestion.js';
+import { InvalidInputError } from '#Errors/invalidInputError';
+import { getOperator } from '#Lib/getOperator';
+import { promptQuestion } from '#Lib/promptQuestion';
 
-// 1º captura entrada de usuario por consola
 (async () => {
-    const userAnswer = await promptQuestion('Introduce tu operación: ');
-    console.log(userAnswer);
+    try {
+        // 1º captura entrada de usuario por consola
+        const userAnswer = await promptQuestion('Introduce tu operación: ');
+
+        // 2º Validar enrada y separar las partes de la misma en operandos y operaciones
+        const standarizeInput = userAnswer.trim();
+        if (standarizeInput === '') throw new InvalidInputError();
+
+        const operator = getOperator(standarizeInput);
+
+        if (!operator) throw new InvalidInputError();
+    } catch (error) {
+        if (error instanceof InvalidInputError) console.log(error.message);
+        else
+            console.log(
+                `Error no controlado: ${error.message}. Stack: ${error.stack}`
+            );
+    }
+    // 3º Realizar la operación
+    // 4º Mostrar resultado por consola
 })();
-// 2º Validar enrada y separar las partes de la misma en operandos y operaciones
-// 3º Realizar la operación
-// 4º Mostrar resultado por consola
